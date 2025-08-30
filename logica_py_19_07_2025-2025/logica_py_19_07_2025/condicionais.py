f3ea7efe59e34f39689401cf8b9a309de381a259
KevinJota <141651714+KevinJota@users.noreply.github.com>
2025-08-30 09:34:33 -0300

    Delete logica_py_19_07_2025-2025 directory

--- logica_py_19_07_2025-2025/logica_py_19_07_2025/condicionais.py
+++
@@ -1,9 +0,0 @@
-nota1 = float(input("digite a nota do aluno: "))
-# nota2 = float(input("digite a nota do aluno: "))
-
-if  nota1 >= 7:
-    print("APROVADO! PARABÉNS")
-elif nota1 >= 4:
-    print("TÁ DE RECUPERAÇÃO")
-else:
-    print("REPROVADO!")
